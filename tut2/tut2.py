4f6d867c404b1f833575118fd32e47f859ee0df2
mohithsaran <mohithsaran.kanamarlapudi@gmail.com>
2024-05-15 16:41:30 +0530

    Added tut2 which uses render_template

---
+++ tut2/tut2.py
@@ -0,0 +1,14 @@
+from flask import Flask,redirect,url_for,render_template
+
+#Flask constructor takes the name of current module
+app=Flask(__name__)
+
+#route function of the flask class
+#which tells the app which URL to call
+#Default - GET request
+@app.route("/<name>")
+def home(name):
+    return  render_template('index.html',content=name,row=2)
+
+if __name__=="__main__":
+    app.run()
