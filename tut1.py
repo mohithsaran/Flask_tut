1eb2b21b8c052af2f034b2c6c7c024f25dbca6d1
mohithsaran <mohithsaran.kanamarlapudi@gmail.com>
2024-05-15 14:45:13 +0530

    Hello world tut in flask

---
+++ tut1.py
@@ -0,0 +1,17 @@
+from flask import Flask
+
+#Flask constructor takes the name of current module
+app=Flask(__name__)
+
+#route function of the flask class
+#which tells the app which URL to call
+#Default - GET request
+@app.route("/")
+def home():
+    return "Hello World <h1>Hello</h1>"
+
+
+#main driver function
+if __name__=="__main__":
+    #to run the flask application on local server 5000 port
+    app.run()
