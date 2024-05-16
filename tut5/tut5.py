dc50568f098a875de871d8b99836fc8d1b5916f5
mohithsaran <mohithsaran.kanamarlapudi@gmail.com>
2024-05-16 16:12:16 +0530

    Added a secret key and created a session

--- tut5/tut5.py
+++ tut5/tut5.py
@@ -1,6 +1,8 @@
 from flask import Flask,render_template,request,url_for,redirect,session
 
 app=Flask(__name__)
+#setting a secret key
+app.secret_key="hello"
 
 @app.route("/")
 def home():
@@ -10,17 +12,20 @@ def home():
 def login():
     if request.method == 'POST':
         user=request.form['name']
-        session['user']=user
-        return redirect(url_for('user'))
+        # assigning user to session dictionary
+        session['user']=user 
+        return redirect(url_for('user',user=user))
     else:    
         return render_template("login.html")
 
-@app.route("/<usr>")
-def user(usr):
+@app.route("/<user>")
+def user(user):
     if "user" in session:
-        usr=session["user"]
-        return f"<h1>{usr}</h1>"
+        # fetching user from session and assigning to usr variable
+        user=session['user']
+        return f"<h1>{user}</h1>"
     else:
+        # if session is completed then redirect to login
         return redirect(url_for('login'))
 
 if __name__=="__main__":
